6029b0c4f996d7492ee1aefcc8204880d8c5892a
Ikrombek14 <94869709+Ikrombek14@users.noreply.github.com>
2024-05-12 12:29:21 +0500

    ewwe

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#@81&nn-&st%ndr!2)2k=0d!^tw59(a4cz++#3vk%b=emr@6(@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
